9d67101c51e57c30d55b83ec50128ae0783274b7
Marcos Bernard <marcoscalixto92@gmail.com>
2022-09-27 14:51:14 -0500

    streamlit v0.1, botón y gráfico

--- app.py
+++ app.py
@@ -11,42 +11,51 @@ import plotly.graph_objects as go
 st.write("""
 # FTX Market Trading app
 """)
-st.write("""
-## 1. ETH/US
-""")
-
-api_url = 'https://ftx.us/api'
-api = '/markets'
-url = api_url+api
-markets = requests.get(url).json()
-
-# Historical data Etherium
-market_name = 'ETH/USD' # Select cryto currency
-resolution = 60*60*24*30 # Save time seconds
-#start = datetime.datetime(2022,1,1).timestamp()
-start = datetime.datetime(2022,1,1).timestamp()
-finish = datetime.datetime(2022,2,1).timestamp()
-
-path = f'/markets/{market_name}/candles?resolution={resolution}&start={start}&finish={finish}'
-url = api_url + path
-res = requests.get(url).json()
-df = pd.DataFrame(res['result'])
-df['date'] = pd.to_datetime(df['startTime'])
-
-df = df.drop(columns=['startTime', 'time'])
-
-fig = go.Figure(data=[go.Candlestick(x=df['date'],
-                open=df['open'],
-                high=df['high'],
-                low=df['low'],
-                close=df['close'])])
-fig.show()
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
 
 
-# # Historical data Etherium
-# market_name = 'ÑBTC/USD' # Select cryto currency
+def historical_data():
+    st.write("""
+    ## 1. ETH/US
+    """)
+    api_url = 'https://ftx.us/api'
+    api = '/markets'
+    url = api_url+api
+    markets = requests.get(url).json()
+
+    # Historical data Etherium
+    market_name = 'ETH/USD' # Select cryto currency
+    resolution = 60*60*24 # Save time seconds
+    #start = datetime.datetime(2022,1,1).timestamp()
+    start = datetime.datetime(2022,2,1).timestamp()
+
+    path = f'/markets/{market_name}/candles?resolution={resolution}&start={start}'
+    url = api_url + path
+
+    res = requests.get(url).json()
+    df = pd.DataFrame(res['result'])
+    df['date'] = pd.to_datetime(df['startTime'])
+
+    df = df.drop(columns=['startTime', 'time'])
+
+    fig = go.Figure(data=[go.Candlestick(x=df['date'],
+                    open=df['open'],
+                    high=df['high'],
+                    low=df['low'],
+                    close=df['close'])])
+    fig.show()
+    # Plot!
+    return(st.plotly_chart(fig, use_container_width=True))
+    
+st.title("Making a Button")
+result = st.button("Click Here")
+st.write(result)
+
+if result:
+    st.write(":smile:")
+    historical_data()
+
+# # Historical data BTC/USD
+# market_name = 'BTC/USD' # Select cryto currency
 # resolution = 60*60*24*30 # Save time seconds
 # start = datetime.datetime(2022,1,1).timestamp()
 # path = f'/markets/{market_name}/candles?resolution={resolution}&start={start}'
